c6e81e9dfbcfc72e3c81678be414712e58c4fe77
Joao Gabriel <joaogabriel9918@gmail.com>
2026-08-07 16:46:01 -0300

    feat: Customização do pipe com mensagem de erro personalizado

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CustomParseIntPipe } from '../common/pipes/custom-parse-int-pipe.pipe';
 
 @Controller('user')
 export class UserController {
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', CustomParseIntPipe) id: number) {
     return `olá do controller do user #${id}`;
   }
 }
