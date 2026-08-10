9875c6eb9e1b55cc69fdad2d62b68168303232a1
Joao Gabriel <joaogabriel9918@gmail.com>
2026-08-10 18:25:58 -0300

    feat: Assinatura do JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { UserService } from '../user/user.service';
+import { HashingService } from '../common/hashing/hashing.service';
+import { JwtService } from '@nestjs/jwt';
+import { JWTPayload } from './types/jwt-payload.type';
 
 @Injectable()
 export class AuthService {
-  doLogin(loginDto: LoginDto) {
-    return loginDto;
+  constructor(
+    private readonly userService: UserService,
+    private readonly hashingService: HashingService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(loginDto: LoginDto) {
+    const user = await this.userService.findByEmail(loginDto.email);
+    const error = new UnauthorizedException('Usuario ou senha invalidos');
+
+    if (!user) {
+      throw error;
+    }
+
+    const isPasswordValid = await this.hashingService.compare(
+      loginDto.password,
+      user.password,
+    );
+
+    if (!isPasswordValid) {
+      throw error;
+    }
+
+    const jwtPayload: JWTPayload = {
+      sub: user.id,
+      email: user.email,
+    };
+    const acessToken = await this.jwtService.signAsync(jwtPayload);
+
+    user.forceLogout = false;
+    await this.userService.save(user);
+
+    return { acessToken };
   }
 }
