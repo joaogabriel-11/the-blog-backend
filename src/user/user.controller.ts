a0d302ab7cb874dc043f46fed828d5911232a19d
Joao Gabriel <joaogabriel9918@gmail.com>
2026-08-10 10:08:27 -0300

    feat: Criando base de dadaos em sqlite e conectando ao projeto

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { CustomParseIntPipe } from '../common/pipes/custom-parse-int-pipe.pipe';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly configService: ConfigService) {}
+
   @Get(':id')
   findOne(@Param('id', CustomParseIntPipe) id: number) {
+    console.log(process.env.TESTE);
+    console.log(this.configService.getOrThrow('TESTE'));
     return `olá do controller do user #${id}`;
   }
 }
