4a7cd3f0b6c6b681fa01eb0842dcc97e4626f254
Joao Gabriel <joaogabriel9918@gmail.com>
2026-08-12 08:43:31 -0300

    feat: Criando DTO do Post

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,1 +1,25 @@
-export class CreatePostDto {}
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Length,
+} from 'class-validator';
+
+export class CreatePostDto {
+  @IsString({ message: 'Titulo precisa ser uma string' })
+  @Length(10, 150, { message: 'Titulo precisa ter entre 10 e 150 caracteres' })
+  title: string;
+
+  @IsString({ message: 'Excerto precisa ser uma string' })
+  @Length(10, 200, { message: 'Excerto precisa ter entre 10 e 200 caracteres' })
+  excerpt: string;
+
+  @IsString({ message: 'Conteudo precisa ser uma string' })
+  @IsNotEmpty({ message: 'Conteudo nao pode ser vazio' })
+  content: string;
+
+  @IsOptional()
+  @IsUrl({ require_tld: false })
+  coverImageUrl?: string;
+}
